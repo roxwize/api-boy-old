d35768394d50b075821c7fd1b41fa7abcceae14e
hoylecake <thekidcorner@gmail.com>
2020-08-22 00:10:55 -0400

    new api
    dog facts
    
    expect some full commands for this stuff soon (such as dog and cat images along with the facts :sunglasses:)

--- main.js
+++ main.js
@@ -49,13 +49,15 @@ client.on('message', async message => {
 	if (command === 'apitest') {
         const { fact } = await fetch('http://catfact.ninja/fact').then(response => response.json());
         const { text } = await fetch('https://uselessfacts.jsph.pl/random.json?language=en').then(response => response.json());
+        const { facts } = await fetch('https://dog-api.kinduff.com/api/facts').then(response => response.json());
 
         const embed = new MessageEmbed()
             .setColor('#d1bf66')
             .setTitle('api testing')
             .addFields(
                 { name: 'catfact.ninja', value: fact },
-                { name: 'uselessfacts.jsph.pl', value: text}
+                { name: 'uselessfacts.jsph.pl', value: text},
+                { name: 'dog-api.kinduff.com', value: facts}
             );
 
             message.channel.send(embed);
